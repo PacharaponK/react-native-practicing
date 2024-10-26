41ceb193084be267d61400baa0c5fef918af8d1d
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-10-27 00:52:29 +0700

    onboarding screen

---
+++ components/CustomButton.jsx
@@ -0,0 +1,33 @@
+import { TouchableOpacity, Text, View } from "react-native";
+import React from "react";
+
+const CustomButton = ({
+	title,
+	handlePress,
+	containerStyles,
+	textStyles,
+	isLoading,
+}) => {
+	return (
+		<TouchableOpacity
+			onPress={handlePress}
+			activeOpacity={0.7}
+			className={`${containerStyles} ${isLoading ? "opacity-50" : ""}`}
+			disabled={isLoading}
+			style={{
+				backgroundColor: "#FF9C01",
+				borderRadius: 10,
+				minHeight: 62,
+				justifyContent: "center",
+				alignItems: "center",
+				paddingHorizontal: 10,
+			}}
+		>
+			<Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+				{title}
+			</Text>
+		</TouchableOpacity>
+	);
+};
+
+export default CustomButton;
